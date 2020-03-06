8b6f8da4e63152e472afd811096594fd9ea3a721
Evgenii Shumilov <mechnotech@yandex.ru>
2020-03-06 16:27:55 +0300

    маленький коммитик

--- hashpass.py
+++ hashpass.py
@@ -2,11 +2,11 @@ import hashlib
 import os
 
 
-#pas = getpass.getpass("Пароль для шифровки :")
+
 print('Результат появится в буфере обмена, можно вставить в браузере Ctrl + V')
-pas = input("Ввеите фразу для шифровки :")
-pas = pas + "secretphrase12"  # Секретная соль, нужно изменить под себя
-result = hashlib.sha224(pas.encode()).hexdigest()
+password = input("Ввеите фразу для шифровки :")
+password = password + "secretphrase12"  # Секретная соль, нужно изменить под себя
+result = hashlib.sha224(password.encode()).hexdigest()
 result = 'V!6' + result[5:21]  # Эти параметры тоже можно изменить, добавив спецсимволы, изменив размер среза
 print('Результат в буфере обмена, можно вставить в браузере Ctrl + V')
 cmd = 'echo ' + str(result) + '| clip'
