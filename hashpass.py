7de62c836cc68091623ac5664856c6bc7f77409e
Evgenii Shumilov <mechnotech@yandex.ru>
2020-03-09 15:27:09 +0300

    Приведем к функции

--- hashpass.py
+++ hashpass.py
@@ -2,15 +2,17 @@ import hashlib
 import os
 
 
+def hash_pass(password):
+    print('Результат появится в буфере обмена, можно вставить в браузере Ctrl + V')
+    password += "secretphrase12"  # Секретная соль, нужно изменить под себя
+    result = hashlib.sha224(password.encode()).hexdigest()
+    result = 'V!6' + result[5:21]  # Эти параметры тоже можно изменить, добавив спецсимволы, изменив размер среза
+    print('Результат в буфере обмена, можно вставить в браузере Ctrl + V')
+    cmd = 'echo ' + str(result) + '| clip'
+    os.system(cmd)
+    #input('Нажмите ENTER для выхода')
 
-print('Результат появится в буфере обмена, можно вставить в браузере Ctrl + V')
-password = input("Ввеите фразу для шифровки :")
-password = password + "secretphrase12"  # Секретная соль, нужно изменить под себя
-result = hashlib.sha224(password.encode()).hexdigest()
-result = 'V!6' + result[5:21]  # Эти параметры тоже можно изменить, добавив спецсимволы, изменив размер среза
-print('Результат в буфере обмена, можно вставить в браузере Ctrl + V')
-cmd = 'echo ' + str(result) + '| clip'
-os.system(cmd)
-#input('Нажмите ENTER для выхода')
+
+hash_pass(input("Ввеите фразу для шифровки :"))
 
 
